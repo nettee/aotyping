9a76f5aafa40ae6183f01fea78e0c1c908e17a66
William <nettee.liu@gmail.com>
2018-01-22 19:32:05 +0800

    add soft keyboard

---
+++ app/src/main/java/com/example/admin/mykeyboardview/AllKeyboardUtil.java
@@ -0,0 +1,201 @@
+package com.example.admin.mykeyboardview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.os.Build;
+import android.text.Editable;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * Created by admin on 2017/8/27.
+ */
+
+public class AllKeyboardUtil {
+    public boolean isupper = false;// 是否大写
+    boolean isShow=false;
+    private EditText ed;
+    private Activity mActivity;
+    private KeyboardView mKeyboardView;
+    private Keyboard       mKeyboardNumber;//键盘
+    private EditText       mEditText;
+
+    public AllKeyboardUtil(Activity activity) {
+        this.mActivity = activity;
+        mKeyboardNumber = new Keyboard(mActivity, R.xml.custom_keyboardnumber);
+        mKeyboardView = (MyAllKeyBoardView) mActivity.findViewById(R.id.keyboard_view);
+    }
+
+    /**
+     * edittext绑定自定义键盘
+     *
+     * @param editText 需要绑定自定义键盘的edittext
+     */
+    public void attachTo(EditText editText) {
+        this.mEditText = editText;
+        hideSystemSofeKeyboard(mActivity.getApplicationContext(), mEditText);
+        showSoftKeyboard();
+    }
+    public void showSoftKeyboard() {
+        if (mKeyboardNumber == null) {
+            mKeyboardNumber = new Keyboard(mActivity, R.xml.custom_keyboardnumber);
+        }
+        if (mKeyboardView == null) {
+            mKeyboardView = (MyKeyBoardView) mActivity.findViewById(R.id.keyboard_view);
+        }
+        mKeyboardView.setKeyboard(mKeyboardNumber);
+
+        mKeyboardView.setEnabled(true);
+        mKeyboardView.setPreviewEnabled(false);
+        mKeyboardView.setVisibility(View.VISIBLE);
+        mKeyboardView.setOnKeyboardActionListener(listener);
+
+    }
+    private KeyboardView.OnKeyboardActionListener listener = new KeyboardView.OnKeyboardActionListener() {
+        @Override
+        public void swipeUp() {
+        }
+
+        @Override
+        public void swipeRight() {
+        }
+
+        @Override
+        public void swipeLeft() {
+        }
+
+        @Override
+        public void swipeDown() {
+        }
+
+        @Override
+        public void onText(CharSequence text) {
+        }
+
+        @Override
+        public void onRelease(int primaryCode) {
+        }
+
+        @Override
+        public void onPress(int primaryCode) {
+        }
+
+        @Override
+        public void onKey(int primaryCode, int[] keyCodes) {
+            Editable editable = mEditText.getText();
+            int start = mEditText.getSelectionStart();
+            if (primaryCode == Keyboard.KEYCODE_CANCEL) {// 完成
+                hideKeyboard();
+            } else if (primaryCode == Keyboard.KEYCODE_DELETE) {// 回退
+                if (editable != null && editable.length() > 0) {
+                    if (start > 0) {
+                        editable.delete(start - 1, start);
+                    }
+                }
+            } else if (primaryCode == Keyboard.KEYCODE_SHIFT) {// 大小写切换
+                changeKey();
+                mKeyboardView.setKeyboard(mKeyboardNumber);
+            } else if (primaryCode == 57419) { // go left
+                if (start > 0) {
+                    mEditText.setSelection(start - 1);
+                }
+            } else if (primaryCode == 57421) { // go right
+                if (start < mEditText.length()) {
+                    mEditText.setSelection(start + 1);
+                }
+            } else {
+                editable.insert(start, Character.toString((char) primaryCode));
+            }
+        }
+    };
+
+    /**
+     * 键盘大小写切换
+     */
+    private void changeKey() {
+        List<Keyboard.Key> keylist = mKeyboardNumber.getKeys();
+        if (isupper) {//大写切换小写
+            isupper = false;
+            for(Keyboard.Key key:keylist){
+                if (key.label!=null && isword(key.label.toString())) {
+                    key.label = key.label.toString().toLowerCase();
+                    key.codes[0] = key.codes[0]+32;
+                }else if(key.label.toString().equals("小写")){
+                    key.label="大写";
+                }
+
+            }
+        } else {//小写切换大写
+            isupper = true;
+            for(Keyboard.Key key:keylist){
+                if (key.label!=null && isword(key.label.toString())) {
+                    key.label = key.label.toString().toUpperCase();
+                    key.codes[0] = key.codes[0]-32;
+                }  else if(key.label.toString().equals("大写")){
+                    key.label="小写";
+                }
+            }
+        }
+    }
+
+    public void showKeyboard() {
+        int visibility = mKeyboardView.getVisibility();
+        if (visibility == View.GONE || visibility == View.INVISIBLE) {
+            mKeyboardView.setVisibility(View.VISIBLE);
+            isShow=true;
+        }
+    }
+
+    public void hideKeyboard() {
+        int visibility = mKeyboardView.getVisibility();
+        if (visibility == View.VISIBLE) {
+            mKeyboardView.setVisibility(View.INVISIBLE);
+            isShow=false;
+        }
+    }
+
+    private boolean isword(String str){
+        String wordstr = "abcdefghijklmnopqrstuvwxyz";
+        if (wordstr.indexOf(str.toLowerCase())>-1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 隐藏系统键盘
+     *
+     * @param editText
+     */
+    public static void hideSystemSofeKeyboard(Context context, EditText editText) {
+        int sdkInt = Build.VERSION.SDK_INT;
+        if (sdkInt >= 11) {
+            try {
+                Class<EditText> cls = EditText.class;
+                Method setShowSoftInputOnFocus;
+                setShowSoftInputOnFocus = cls.getMethod("setShowSoftInputOnFocus", boolean.class);
+                setShowSoftInputOnFocus.setAccessible(true);
+                setShowSoftInputOnFocus.invoke(editText, false);
+
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            editText.setInputType(InputType.TYPE_NULL);
+        }
+        // 如果软键盘已经显示，则隐藏
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+}
---
+++ app/src/main/java/com/example/admin/mykeyboardview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.admin.mykeyboardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.editText_word);
+        final AllKeyboardUtil keyboardUtil = new AllKeyboardUtil(MainActivity.this);
+
+        mEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyboardUtil.attachTo(mEditText);
+            }
+        });
+    }
+}
